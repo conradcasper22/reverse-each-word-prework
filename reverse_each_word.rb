5b60f007aa266f7bc4ba5adec749ed72711d6970
Conrad Casper <conradcasper@icloud.com>
2019-05-30 21:47:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,14 @@ require "pry"
 
 def reverse_each_word(sentence)
   array = Array.new
-  split_array = sentence.split("")
+  split_array = sentence.split(",")
   # binding.pry
   reversed_array = split_array.reverse
   # binding.pry
   reversed_array.each do |x|
     array << x
   end
-  array
+  array.split
   binding.pry
 end
 
