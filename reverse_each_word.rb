862bfacd813cfd732cbac73441d5e874cd523a7a
Conrad Casper <conradcasper@icloud.com>
2019-05-30 22:08:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require "pry"
 def reverse_each_word(sentence)
   
   split_str = sentence.split(" ")
-  split_str.collect do |x|
-    new_array << x.reverse
+  new_array = split_str.collect do |x|
+     x.reverse
 end
 answer = new_array.join(" ")
 end
