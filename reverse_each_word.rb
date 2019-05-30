854893fd50ebe987a49ca5247c7856d538a9af46
Conrad Casper <conradcasper@icloud.com>
2019-05-30 21:51:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,7 @@
 require "pry"
 
 def reverse_each_word(sentence)
-  reversed_array = Array.new
-  split_array = sentence.split(",")
-  # binding.pry
-  reversed_array = split_array.reverse
-  # binding.pry
-  reversed_array.each do |x|
-    reversed_array << x
-  end
-  array
   
-  binding.pry
 end
 
 
